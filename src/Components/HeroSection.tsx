08d42e6ab793c8ff21b0efe274da8a3d9ec1d1b1
Kumar Keshav <keshavraina737@gmail.com>
2024-04-03 02:04:05 +0530

    first

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -6,8 +6,8 @@ function HeroSection() {
   return (
     <div className="relative bg-black-500 min-h-[40rem] md:h-auto flex items-center justify-center overflow-hidden rounded-md mt-12"> {/* Changed background color to bg-blue-500 */}
       <div className="max-w-5xl mx-auto px-6 md:px-12 py-20 md:py-32 text-center text-white">
-        <h1 className="text-6xl md:text-6xl font-bold mb-4">Welcome to Our Shopping Platform</h1>
-        <p className="text-lg md:text-xl mb-8">Discover the best deals on garments, groceries, and electrical products.</p>
+        <h1 className="text-6xl md:text-6xl font-bold mb-4">Welcome to our online shopping platform</h1>
+        <p className="text-lg md:text-xl mb-8">Explore the finest offers on garement, groceries, and electrical items..</p>
         <button
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out inline-flex items-center">
           <span>Shop Now</span>
